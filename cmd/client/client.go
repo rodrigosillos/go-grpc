d35665b3e5d7f48ebfafdb32bd8144c8506b9e62
Rodrigo Sillos <rodrigosillos@gmail.com>
2021-08-10 11:10:00 -0300

    add new user

---
+++ cmd/client/client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/rodrigosillos/go-grpc/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+	// AddUserVerbose(client)
+	// AddUsers(client)
+	// AddUserStreamBoth(client)
+
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Sillos",
+		Email: "sillos@sillos.com",
+	}
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+	fmt.Println(res)
+}
---
+++ cmd/server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/rodrigosillos/go-grpc/pb"
+	"github.com/rodrigosillos/go-grpc/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, &services.UserService{})
+	// reflection.Register(grpcServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Could not serve: %v", err)
+	}
+}
